6b64422d1bd48cd59b4d68cf49efef9473ed3350
Pavel Sarmiento <pavel.sarmiento.kochersky@student.forsbergsskola.se>
2021-11-09 13:41:04 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System;
 using GLFW;
 using static OpenGL.Gl;
 
@@ -7,16 +6,24 @@ namespace SharpEngine
 {
     class Program
     {
-        static void Main(string[] args) {
+        static void Main(string[] args)
+        {
             Glfw.Init();
+            Glfw.WindowHint(Hint.ClientApi, ClientApi.OpenGL);
+            Glfw.WindowHint(Hint.ContextVersionMajor, 3);
+            Glfw.WindowHint(Hint.ContextVersionMinor, 3);
+            Glfw.WindowHint(Hint.Decorated, true);
+            Glfw.WindowHint(Hint.OpenglProfile, Profile.Core);
+            Glfw.WindowHint(Hint.OpenglForwardCompatible, Constants.True);
 
-            var window = Glfw.CreateWindow(1024, 768, "SharpEngine", Monitor.None, Window.None);
+            var window = Glfw.CreateWindow(1024, 768, "SharpEngine",Monitor.None, Window.None);
             Glfw.MakeContextCurrent(window);
             Import(Glfw.GetProcAddress);
 
-            float[] vertices = new float[] {
+            float[] vertices = new float[]
+            {
                 -.5f, -.5f, 0f,
-                .5f, -.5f, 0f,
+                .5f, -.5f, 0f, 
                 0f, .5f, 0f
             };
 
@@ -24,16 +31,19 @@ namespace SharpEngine
             var vertexBuffer = glGenBuffer();
             glBindVertexArray(vertexArray);
             glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-            unsafe {
-                fixed (float* vertex = &vertices[0]) {
+            unsafe
+            {
+                fixed (float * vertex = &vertices[0])
+                {
                     glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices.Length, vertex, GL_STATIC_DRAW);
                 }
                 glVertexAttribPointer(0, 3, GL_FLOAT, false, 3 * sizeof(float), NULL);
             }
             glEnableVertexAttribArray(0);
 
-            while (!Glfw.WindowShouldClose(window)) {
-                Glfw.PollEvents(); // react to window changes (position etc.)
+            while (!Glfw.WindowShouldClose(window))
+            {
+                Glfw.PollEvents(); //reacts to window changes (position etc.)
                 glDrawArrays(GL_TRIANGLES, 0, 3);
                 Glfw.SwapBuffers(window);
             }
