818535636e8d0159f92732e8c176396d9f8aeace
Pavel Sarmiento <pavel.sarmiento.kochersky@student.forsbergsskola.se>
2021-11-11 14:40:28 +0100

    Fixed working code

--- Program.cs
+++ Program.cs
@@ -1,126 +1,119 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
+using System.Numerics;
 using GLFW;
 using static OpenGL.Gl;
 
 namespace SharpEngine
 {
-    struct Vector
-    {
-        public float x, y, z;
-
-        public Vector(float x, float y, float z)
-        {
-            this.x = x;
-            this.y = y;
-            this.z = z;
-        }
-
-        public Vector(float x, float y)
-        {
-            this.x = x;
-            this.y = y;
-            this.z = 0;
-        }
-
-        public static Vector operator *(Vector v, float f)
-        {
-            return new Vector(v.x * f, v.y * f, v.z * f);
-        }
-
-        public static Vector operator +(Vector v, Vector v2)
-        {
-            return new Vector(v.x + v2.x, v.y + v2.y, v.z + v2.z);
-        }
-
-        public static Vector operator -(Vector v, Vector v2)
-        {
-            return new Vector(v.x - v2.x, v.y - v2.y, v.z - v2.z);
-        }
-
-        public static Vector operator /(Vector v, float f)
-        {
-            return new Vector(v.x / f, v.y / f, v.z / f);
-        }
-    }
-    
     class Program
     {
-        static Vector[] vertices = new Vector[] {
-            new Vector(-.1f, -.1f),
-            new Vector(.1f, -.1f),
-            new Vector(0f, .1f),
-            new Vector(.4f, .4f),
-            new Vector(.6f, .4f),
-            new Vector(.5f, .6f),
-            
+        
+        static Vertex[] vertices = new Vertex[] {
+            // new Vector(-.1f, -.1f),
+            // new Vector(.1f, -.1f),
+            // new Vector(0f, .1f),
+            new Vertex(new Vector(0f, 0f), Color.Red),
+            new Vertex(new Vector(1f, 0f), Color.Green),
+            new Vertex(new Vector(0f, 1f), Color.Blue)
         };
         
-        
         static void Main(string[] args) {
+            
             var window = CreateWindow();
 
             LoadTriangleIntoBuffer();
 
             CreateShaderProgram();
 
-
-            
             // engine rendering loop
-            var direction = new Vector(0.001f, 0.001f);
+            var direction = new Vector(0.0003f, 0.0003f);
+            var multiplier = 0.999f;
+            var scale = 1f;
             while (!Glfw.WindowShouldClose(window)) {
                 Glfw.PollEvents(); // react to window changes (position etc.)
                 ClearScreen();
                 Render(window);
                 
+                // 1. Scale the Triangle without Moving it
                 
-                for (var i = 0; i < vertices.Length; i ++ )
-                {
-                    vertices[i] += direction;
+                // 1.1 Move the Triangle to the Center, so we can scale it without Side Effects
+                // 1.1.1 Find the Center of the Triangle
+                // 1.1.1.1 Find the Minimum and Maximum
+                var min = vertices[0].position;
+                for (var i = 1; i < vertices.Length; i++) {
+                    min = Vector.Min(min, vertices[i].position);
+                }
+                var max = vertices[0].position;
+                for (var i = 1; i < vertices.Length; i++) {
+                    max = Vector.Max(max, vertices[i].position);
+                }
+                // 1.1.1.2 Average out the Minimum and Maximum to get the Center
+                var center = (min + max) / 2;
+                // 1.1.2 Move the Triangle the Center
+                for (var i = 0; i < vertices.Length; i++) {
+                    vertices[i].position -= center;
+                }
+                // 1.2 Scale the Triangle
+                for (var i = 0; i < vertices.Length; i++) {
+                    vertices[i].position *= multiplier;
+                }
+                // 1.3 Move the Triangle Back to where it was before
+                for (var i = 0; i < vertices.Length; i++) {
+                    vertices[i].position += center;
+                }
+                
+                // 2. Keep track of the Scale, so we can reverse it
+                scale *= multiplier;
+                if (scale <= 0.5f) {
+                    multiplier = 1.001f;
+                }
+                if (scale >= 1f) {
+                    multiplier = 0.999f;
                 }
 
-                for (int i = 0; i < vertices.Length; i++)
-                {
-                    if (vertices[i].x >= 1 || vertices[i].x <= -1)
-                    {
+                // 3. Move the Triangle by its Direction
+                for (var i = 0; i < vertices.Length; i++) {
+                    vertices[i].position += direction;
+                }
+                // 4. Check the X-Bounds of the Screen
+                for (var i = 0; i < vertices.Length; i++) {
+                    if (vertices[i].position.x >= 1 && direction.x > 0 || vertices[i].position.x <= -1 && direction.x < 0) {
                         direction.x *= -1;
                         break;
                     }
                 }
-                for (int i = 0; i < vertices.Length; i++)
-                {
-                    if (vertices[i].y >= 1 || vertices[i].y <= -1)
-                    {
+                // 5. Check the Y-Bounds of the Screen
+                for (var i = 0; i < vertices.Length; i++) {
+                    if (vertices[i].position.y >= 1 && direction.y > 0 || vertices[i].position.y <= -1 && direction.y < 0) {
                         direction.y *= -1;
                         break;
                     }
                 }
-                
+
                 UpdateTriangleBuffer();
             }
         }
 
-        private static void ClearScreen()
-        {
-            glClearColor(.2f, .05f, .2f, 1);
-            glClear(GL_COLOR_BUFFER_BIT);
-        }
-
-        private static void Render(Window window)
-        {
+        static void Render(Window window) {
             glDrawArrays(GL_TRIANGLES, 0, vertices.Length);
             Glfw.SwapBuffers(window);
+            //glFlush();
+        }
+
+        static void ClearScreen() {
+            glClearColor(.2f, .05f, .2f, 1);
+            glClear(GL_COLOR_BUFFER_BIT);
         }
 
         static void CreateShaderProgram() {
             // create vertex shader
             var vertexShader = glCreateShader(GL_VERTEX_SHADER);
-            glShaderSource(vertexShader, File.ReadAllText("shaders/screen-coordinates.vert"));
+            glShaderSource(vertexShader, File.ReadAllText("shaders/position-color.vert"));
             glCompileShader(vertexShader);
 
             // create fragment shader
             var fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-            glShaderSource(fragmentShader, File.ReadAllText("shaders/green.frag"));
+            glShaderSource(fragmentShader, File.ReadAllText("shaders/vertex-color.frag"));
             glCompileShader(fragmentShader);
 
             // create shader program - rendering pipeline
@@ -132,21 +125,20 @@ namespace SharpEngine
         }
 
         static unsafe void LoadTriangleIntoBuffer() {
-
-            // load the vertices into a buffer
             var vertexArray = glGenVertexArray();
             var vertexBuffer = glGenBuffer();
             glBindVertexArray(vertexArray);
             glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
             UpdateTriangleBuffer();
-            glVertexAttribPointer(0, 3, GL_FLOAT, false, sizeof(Vector), NULL);
-
+            glVertexAttribPointer(0, 3, GL_FLOAT, false, sizeof(Vertex), NULL);
+            glVertexAttribPointer(1, 4, GL_FLOAT, false, sizeof(Vertex), (void*) sizeof(Vector));
             glEnableVertexAttribArray(0);
+            glEnableVertexAttribArray(1);
         }
         
         static unsafe void UpdateTriangleBuffer() {
-            fixed (Vector* vertex = &vertices[0]) {
-                glBufferData(GL_ARRAY_BUFFER, sizeof(Vector) * vertices.Length, vertex, GL_DYNAMIC_DRAW);
+            fixed (Vertex* vertex = &vertices[0]) {
+                glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * vertices.Length, vertex, GL_DYNAMIC_DRAW);
             }
         }
 
