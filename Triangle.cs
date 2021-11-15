3f2587d69901a552cd31cf66b69f128d0ebd5dd8
Pavel Sarmiento <pavel.sarmiento.kochersky@student.forsbergsskola.se>
2021-11-15 16:59:19 +0100

    Introducing Matrix math

--- Triangle.cs
+++ Triangle.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 using static OpenGL.Gl;
 
-namespace SharpEngine {
-	public class Triangle {
+namespace SharpEngine 
+{
+	public class Triangle 
+	{
             
 		Vertex[] vertices;
+		private Matrix transform = Matrix.Identity;
 		uint vertexArray;
 		uint vertexBuffer;
 
@@ -13,14 +17,16 @@ namespace SharpEngine {
 
 		public Material material;
             
-		public Triangle(Vertex[] vertices, Material material) {
+		public Triangle(Vertex[] vertices, Material material) 
+		{
 			this.vertices = vertices;
 			this.material = material;
 			LoadTriangleIntoBuffer();
 			this.CurrentScale = 1f;
 		}
 		
-		 unsafe void LoadTriangleIntoBuffer() {
+		 unsafe void LoadTriangleIntoBuffer() 
+		 {
 			vertexArray = glGenVertexArray();
 			vertexBuffer = glGenBuffer();
 			glBindVertexArray(vertexArray);
@@ -32,49 +38,46 @@ namespace SharpEngine {
 			glBindVertexArray(0);
 		}
 
-		public Vector GetMinBounds() {
+		public Vector GetMinBounds() 
+		{
 			var min = this.vertices[0].position;
-			for (var i = 1; i < this.vertices.Length; i++) {
+			for (var i = 1; i < this.vertices.Length; i++) 
+			{
 				min = Vector.Min(min, this.vertices[i].position);
 			}
 			return min;
 		}
             
-		public Vector GetMaxBounds() {
+		public Vector GetMaxBounds() 
+		{
 			var max = this.vertices[0].position;
-			for (var i = 1; i < this.vertices.Length; i++) {
+			for (var i = 1; i < this.vertices.Length; i++) 
+			{
 				max = Vector.Max(max, this.vertices[i].position);
 			}
 
 			return max;
 		}
 
-		public Vector GetCenter() {
+		public Vector GetCenter() 
+		{
 			return (GetMinBounds() + GetMaxBounds()) / 2;
 		}
 
-		public void Scale(float multiplier) {
-			// We first move the triangle to the center, to avoid
-			// the triangle moving around while scaling.
-			// Then, we move it back again.
-			var center = GetCenter();
-			Move(-center);
-			for (var i = 0; i < this.vertices.Length; i++) {
-				this.vertices[i].position *= multiplier;
-			}
-			Move(center);
-
-			this.CurrentScale *= multiplier;
+		public void Scale(float multiplier) 
+		{
+			
 		}
 
-		public void Move(Vector direction) {
-			for (var i = 0; i < this.vertices.Length; i++) {
-				this.vertices[i].position += direction;
-			}
+		public void Move(Vector direction)
+		{
+			
 		}
 
-		public unsafe void Render() {
+		public unsafe void Render() 
+		{
 			this.material.Use();
+			this.material.SetTransform(this.transform);
 			glBindVertexArray(vertexArray);
 			glBindBuffer(GL_ARRAY_BUFFER, this.vertexBuffer);
 			fixed (Vertex* vertex = &this.vertices[0]) {
@@ -84,17 +87,9 @@ namespace SharpEngine {
 			glBindVertexArray(0);
 		}
 
-		public void Rotate(float rotation) {
-			var center = GetCenter();
-			Move(-center);
-			for (int i = 0; i < this.vertices.Length; i++) {
-				var currentRotation = Vector.Angle(this.vertices[i].position);
-				var distance = vertices[i].position.GetMagnitude();
-				var newX = MathF.Cos(currentRotation + rotation);
-				var newY = MathF.Sin(currentRotation + rotation);
-				vertices[i].position = new Vector(newX, newY) * distance;
-			}
-			Move(center);
+		public void Rotate(float rotation) 
+		{
+			
 		}
 	}
 }
