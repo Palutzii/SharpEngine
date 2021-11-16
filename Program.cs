1bf625d7f689cb3bfdb413bf16500d9f4b6248ab
Pavel Sarmiento <pavel.sarmiento.kochersky@student.forsbergsskola.se>
2021-11-16 13:45:03 +0100

    Imporving the code and making it more readable!
    Remember to COMMIT MORE OFTEN PAVEL!

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using GLFW;
 
 namespace SharpEngine
 {
@@ -10,26 +10,9 @@ namespace SharpEngine
             return from + (to - from) * t;
         }
 
-        static float GetRandomFloat(Random random, float min = 0, float max = 1) 
+        static float GetRandomFloat(Random random, float min = 0, float max = 1)
         {
             return Lerp(min, max, (float)random.Next() / int.MaxValue);
-        }
-        
-        static void FillSceneWithTriangles(Scene scene, Material material) 
-        {
-            var random = new Random();
-            for (var i = 0; i < 10; i++) 
-            {
-                var triangle = new Triangle(new Vertex[] 
-                {
-                    new Vertex(new Vector(-.1f, 0f), Color.Red),
-                    new Vertex(new Vector(.1f, 0f), Color.Green),
-                    new Vertex(new Vector(0f, .133f), Color.Blue)
-                }, material);
-                triangle.Rotate(GetRandomFloat(random));
-                triangle.Move(new Vector(GetRandomFloat(random, -1, 1), GetRandomFloat(random, -1, 1)));
-                scene.Add(triangle);
-            }
         }
         
         static void Main(string[] args) 
@@ -39,59 +22,25 @@ namespace SharpEngine
             var material = new Material("shaders/world-position-color.vert", "shaders/vertex-color.frag");
             var scene = new Scene();
             window.Load(scene);
-            
-            //Adds a shit ton of triangles
-            // FillSceneWithTriangles(scene, material);
-            
-            //Adds one triangle only
-            var newTriangle = new Triangle(new Vertex[] {
-                new Vertex(new Vector(-.1f, 0f), Color.Red),
-                new Vertex(new Vector(.1f, 0f), Color.Green),
-                new Vertex(new Vector(0f, .133f), Color.Blue)
-            }, material);
-            scene.Add(newTriangle);
-            
-            // engine rendering loop
-            var direction = new Vector(0.0003f, 0.0003f);
-            var multiplier = 0.999f;
-            var rotation = 0.0005f;
-            while (window.IsOpen()) 
-            {
 
-                // Update Triangles
-                for (var i = 0; i < scene.triangles.Count; i++) 
+            var shape = new Circle(material);
+            scene.Add(shape);
+
+            var ground = new Rectangle(material);
+            ground.Transform.CurrentScale = new Vector(20f, 1f, 1f);
+            ground.Transform.Position = new Vector(0f, -1f);
+            scene.Add(ground);
+
+            // engine rendering loop
+            const int fixedStepNumberPerSecond = 30;
+            const double fixedStepDuration = 1.0 / fixedStepNumberPerSecond;
+            double previousFixedStep = 0.0;
+            while (window.IsOpen()) {
+                while (Glfw.Time > previousFixedStep + fixedStepDuration) 
                 {
-                    var triangle = scene.triangles[i];
-                
-                    // 2. Keep track of the Scale, so we can reverse it
-                    if (triangle.CurrentScale <= 0.5f) 
-                    {
-                        multiplier = 1.001f;
-                    }
-                    if (triangle.CurrentScale >= 1f) 
-                    {
-                        multiplier = 0.999f;
-                    }
+                    previousFixedStep += fixedStepDuration;
                     
-                    triangle.Scale(multiplier);
-                    triangle.Rotate(rotation);
-                
-                    // 4. Check the X-Bounds of the Screen
-                    if (triangle.GetMaxBounds().x >= 1 && direction.x > 0 || triangle.GetMinBounds().x <= -1 && direction.x < 0) 
-                    {
-                        direction.x *= -1;
-                    }
-                
-                    // 5. Check the Y-Bounds of the Screen
-                    if (triangle.GetMaxBounds().y >= 1 && direction.y > 0 || triangle.GetMinBounds().y <= -1 && direction.y < 0) 
-                    {
-                        direction.y *= -1;
-                    }
-                    
-                    
-                    triangle.Move(direction);
                 }
-                
                 window.Render();
             }
         }
